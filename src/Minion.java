d974a92d31b543500ba322a30d0c9981a452797d
100297 <league@WTS7.attlocal.net>
2017-11-01 18:25:08 -0700

    Minions

---
+++ src/Minion.java
@@ -0,0 +1,36 @@
+import javax.swing.JOptionPane;
+
+class Minion {
+	private String name;
+	private int eyes;
+	private String color;
+	private String master;
+
+	public Minion(String name, int eyes, String color, String master) {
+		this.name = name;
+		this.eyes = eyes;
+		this.color = color;
+		this.master = master;
+	}
+
+	public String getName() {
+		return name;
+
+	}
+
+	public int getEyes() {
+		return eyes;
+	}
+
+	public String getColor() {
+		return color;
+	}
+
+	public String getMaster() {
+		return master;
+	}
+
+	public void setMaster(String master) {
+		this.master = master;
+	}
+}
