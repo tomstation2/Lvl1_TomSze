4b3cb83f0bfa70f0a4d35b15ca80cd17bcdd8501
tomstation2 <league@WorthyMini-6.attlocal.net>
2017-11-15 18:28:26 -0800

    SimpleCalculator

--- src/SimpleCalculator.java
+++ src/SimpleCalculator.java
@@ -1,36 +1,98 @@
+import java.awt.TextField;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.awt.event.MouseEvent;
+import java.awt.event.MouseListener;
+
 import javax.swing.JButton;
 import javax.swing.JFrame;
+import javax.swing.JLabel;
 import javax.swing.JPanel;
 import javax.swing.JTextField;
 
-public class SimpleCalculator {
-	public static void main(String[] args) {
 
-	}
-	void buildGUI() {
+public class SimpleCalculator implements ActionListener,MouseListener{
+private	JButton addButton = new JButton();
+private	JButton subButton = new JButton();
+private	JButton multButton = new JButton();
+private	JButton divButton = new JButton();
+JTextField tf1= new JTextField();
+JTextField tf2= new JTextField();
+JLabel ans=new JLabel();
+	 void buildGUI() {
 		JFrame frame = new JFrame();
+		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		JPanel panel = new JPanel();
 		frame.add(panel);
-		frame.setVisible(true);
 		frame.setSize(500, 200);
 		frame.setTitle("Calculator");
-		JButton button1 = new JButton();
-		JButton button2 = new JButton();
-		JButton button3 = new JButton();
-		JButton button4 = new JButton();
-		button1.setBounds(100, 250, 50, 20);
-		button2.setBounds(200, 250, 50, 20);
-		button3.setBounds(300, 250, 50, 20);
-		button4.setBounds(400, 250, 50, 20);
-		button1.setText("add");
-		button2.setText("subtract");
-		button3.setText("multiply");
-		button4.setText("divide");
-		JTextField tf1= new JTextField();
-		JTextField tf2= new JTextField();
-		panel.add(button1);
-		panel.add(button2);
-		panel.add(button3);
-		panel.add(button4);	
+		addButton.setBounds(100, 150, 50, 20);
+		subButton.setBounds(200, 150, 50, 20);
+		multButton.setBounds(300, 150, 50, 20);
+		divButton.setBounds(400, 150, 50, 20);
+		addButton.setText("add");
+		subButton.setText("subtract");
+		multButton.setText("multiply");
+		divButton.setText("divide");
+		addButton.addMouseListener(this);
+		subButton.addMouseListener(this);
+		multButton.addMouseListener(this);
+		divButton.addMouseListener(this);
+		tf1.setText("0");
+		tf2.setText("0");
+		tf1.setBounds(200, 450, 200, 20);
+		tf2.setBounds(300, 450, 200, 20);
+		ans.setText("0");
+		panel.add(tf1);
+		panel.add(tf2);
+		panel.add(addButton);
+		panel.add(subButton);
+		panel.add(multButton);
+		panel.add(divButton);	
+		panel.add(ans);
+		frame.setVisible(true);
+	}
+	@Override
+	public void actionPerformed(ActionEvent e) {
+		// TODO Auto-generated method stub
+		
+	}
+	@Override
+	public void mouseClicked(MouseEvent e) {
+		//System.out.println("click");	
+	}
+	@Override
+	public void mousePressed(MouseEvent e) {
+		// TODO Auto-generated method stub
+		//System.out.println("press");
+		int number1=Integer.parseInt(tf1.getText());
+		int number2=Integer.parseInt(tf2.getText());
+		if(e.getSource()==addButton) {
+			ans.setText(Integer.toString(number1+number2));
+		}
+		if(e.getSource()==subButton) {
+			ans.setText(Integer.toString(number1-number2));
+		}
+		if(e.getSource()==multButton) {
+			ans.setText(Integer.toString(number1*number2));
+		}
+		if(e.getSource()==divButton) {
+			ans.setText(Float.toString(number1/number2));
+		}
+	}
+	@Override
+	public void mouseReleased(MouseEvent e) {
+		// TODO Auto-generated method stub
+		
+	}
+	@Override
+	public void mouseEntered(MouseEvent e) {
+		// TODO Auto-generated method stub
+		
+	}
+	@Override
+	public void mouseExited(MouseEvent e) {
+		// TODO Auto-generated method stub
+		
 	}
 }
---
+++ src/SimpleCalculatorRunner.java
@@ -0,0 +1,7 @@
+
+public class SimpleCalculatorRunner {
+public static void main(String[] args) {
+	SimpleCalculator calculator=new SimpleCalculator();
+	calculator.buildGUI();
+}
+}
