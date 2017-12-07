39b24609a31ab74708b58c5b86eed017e1862979
RobloxYesCaillouNo <jaydenkudo@gmail.com>
2017-12-06 18:29:33 -0800

    JukeBox(IP)

--- src/Jukebox.java
+++ src/Jukebox.java
@@ -1,13 +1,23 @@
+
 // Copyright The League of Amazing Programmers, 2015
+import java.awt.Dimension;
+import java.awt.Image;
+import java.awt.event.ActionListener;
+import java.awt.event.MouseEvent;
+import java.awt.event.MouseListener;
 import java.io.FileInputStream;
 import java.io.FileNotFoundException;
 import java.io.IOException;
 import java.io.InputStream;
 import java.net.URL;
 
+import javax.imageio.ImageIO;
 import javax.swing.Icon;
 import javax.swing.ImageIcon;
+import javax.swing.JButton;
+import javax.swing.JFrame;
 import javax.swing.JLabel;
+import javax.swing.JPanel;
 import javax.swing.SwingUtilities;
 
 import javazoom.jl.decoder.JavaLayerException;
@@ -16,33 +26,120 @@ import javazoom.jl.player.advanced.AdvancedPlayer;
 /* 1. Download the JavaZoom jar from here: http://bit.ly/javazoom
  * 2. Right click your project and add it as an External JAR (Under Java Build Path > Libraries).*/
 
-public class Jukebox implements Runnable {
+public class Jukebox implements Runnable, MouseListener {
+	private JFrame frame = new JFrame();
+	private JButton button1 = new JButton();
+	private JButton button2 = new JButton();
+	private JButton button3 = new JButton();
+	private JButton button4 = new JButton();
+	private JPanel panel = new JPanel();
+	Song AIWFCIY= new Song("Christmas.mp3");
 
 	public static void main(String[] args) {
 		SwingUtilities.invokeLater(new Jukebox());
-		  Song Christmas=new Song("Christmas.mp3");
-		   Christmas.play();
 	}
 
-           public void run() {
-
+	public void run() {
+		Image AIW = null;
+		try {
+			AIW = ImageIO.read(getClass().getResource("AIWFCIY.jpg"));
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		Image LC = null;
+		try {
+			LC = ImageIO.read(getClass().getResource("LastChrismas.jpg"));
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		Image LSN = null;
+		try {
+			LSN = ImageIO.read(getClass().getResource("LilStNick.jpg"));
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		Image WC = null;
+		try {
+			WC = ImageIO.read(getClass().getResource("WCT.jpg"));
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		Dimension d = new Dimension(250, 250);
+		frame.setVisible(true);
+		frame.setSize(1000, 500);
+		frame.setTitle("JUKEBOX");
+		frame.add(panel);
+		panel.add(button1);
+		panel.add(button2);
+		panel.add(button3);
+		panel.add(button4);
+		button1.setPreferredSize(d);
+		button2.setPreferredSize(d);
+		button3.setPreferredSize(d);
+		button4.setPreferredSize(d);
+		frame.pack();
+		button1.setIcon(new ImageIcon(AIW));
+		button2.setIcon(new ImageIcon(LC));
+		button3.setIcon(new ImageIcon(LSN));
+		button4.setIcon(new ImageIcon(WC));
+		button1.addMouseListener(this);
+		button2.addMouseListener(this);
+		button3.addMouseListener(this);
+		button4.addMouseListener(this);
+		
 		// 3. Find an mp3 on your computer or on the Internet.
 		// 4. Create a Song
-        	 
+		
 		// 5. Play the Song
 		/*
-		 * 6. Create a user interface for your Jukebox so that the user can to
-		 * choose which song to play. You can use can use a different button for
-		 * each song, or a picture of the album cover. When the button or album
-		 * cover is clicked, stop the currently playing song, and play the one
-		 * that was selected.
+		 * 6. Create a user interface for your Jukebox so that the user can to choose
+		 * which song to play. You can use can use a different button for each song, or
+		 * a picture of the album cover. When the button or album cover is clicked, stop
+		 * the currently playing song, and play the one that was selected.
 		 */
-          }
+	}
+
 	/* Use this method to add album covers to your Panel. */
 	private JLabel loadImage(String fileName) {
 		URL imageURL = getClass().getResource(fileName);
 		Icon icon = new ImageIcon(imageURL);
 		return new JLabel(icon);
+	}
+
+	@Override
+	public void mouseClicked(MouseEvent e) {
+		// TODO Auto-generated method stub
+		if(e.getSource()==button1) {
+			AIWFCIY.play();
+		}
+	}
+
+	@Override
+	public void mousePressed(MouseEvent e) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void mouseReleased(MouseEvent e) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void mouseEntered(MouseEvent e) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void mouseExited(MouseEvent e) {
+		// TODO Auto-generated method stub
+		
 	}
 
 }
@@ -54,8 +151,7 @@ class Song {
 	private InputStream songStream;
 
 	/**
-	 * Songs can be constructed from files on your computer or Internet
-	 * addresses.
+	 * Songs can be constructed from files on your computer or Internet addresses.
 	 * 
 	 * Examples: <code> 
 	 * 		new Song("everywhere.mp3"); 	//from default package 
@@ -134,4 +230,3 @@ class Song {
 		}
 	}
 }
-
