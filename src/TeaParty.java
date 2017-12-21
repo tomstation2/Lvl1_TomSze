5b4812d05b5442c049358ed30a523bc623e0cca0
Garrett-Thomas <league@WTS5.attlocal.net>
2017-12-20 18:27:21 -0800

    TeaParty(IP)

--- src/TeaParty.java
+++ src/TeaParty.java
@@ -1,7 +1,19 @@
 public class TeaParty {
 
 	public String welcome(String name, boolean isWoman, boolean isKnighted) {
-
+		if (isWoman == true) {
+			return "Hello Ms. " + name;
+		} else if (isKnighted == true) {
+			return "Hello Sir " + name;
+		} else {
+			return "Hello Mr. " + name;
+		}
 	}
 
+	public static void main(String[] args) {
+		TeaParty party = new TeaParty();
+		party.welcome("George Orwell", false, false);
+		party.welcome("Jane Austen", true, false);
+		party.welcome("Issac Newton", false, true);
+	}
 }
